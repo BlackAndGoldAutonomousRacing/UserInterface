92b5a781c6d31a3d50dc49c70c5000022b9e5f3d
samiam567 <35432488+samiam567@users.noreply.github.com>
2021-10-14 13:47:22 -0400

    Moved Code into a ROS2 Package

---
+++ UserInteface/UserInterface_driver.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from UserInteface.')
+
+
+if __name__ == '__main__':
+    main()
--- UserInteface/userInterfaceRunner.py
+++ UserInteface/userInterfaceRunner.py
@@ -86,90 +86,93 @@ diagonal = sqrt(screenSizeY**2 + screenSizeX**2);
 objectDraw = ObjectDraw(screenSizeX,screenSizeY,frameCaption="Black and Gold Autonomous Racing Control Suite");
 
 
-
-# create background image
-print(diagonal);
-background = Sprite("backgroundlogo",screenSizeX/2,screenSizeY*0.45,scaling=diagonal/1000,imgSource="assets/Blackandgoldlogo.png");
-objectDraw.setBackgroundColor((0,0,0));
-objectDraw.add(background);
-
-
-
-
-
-
-# create pointless add-ons to make it look cooler
-hologram1 = Sprite("hologram_rotator_1", screenSizeX*0.9,screenSizeY*0.7,diagonal/9000,"assets/hologram rotator.png");
-hologram1.setAngularVelocity(0.5);
-objectDraw.add(hologram1);
-
-hologram2 = Sprite("hologram_rotator_2", screenSizeX*0.07,screenSizeY*0.17,diagonal/10000,"assets/hologram_radar.jpg");
-hologram2.setAngularVelocity(-0.7);
-objectDraw.add(hologram2);
-
-
-# create buttons and text messages
-
-
-# title, command
-buttonsDict = {
-    "CT11": ("echo 'command send ct11'",""),
-    "launch lidar": ("echo 'launch lidar'","echo 'stop lidar'"),
-    "launch long control": ("echo 'long_control set true'","echo 'long_control set false'")
-};
-
-createButtons(buttonsDict,objectDraw);
-
-velocityButton = IAC_textBox_Button("set target velocity",objectDraw, screenSizeX*0.5,screenSizeY*0.75,200,screenSizeY*(1-screenSplitButton));
-
-objectDraw.add(velocityButton);
-
-
-# topicname, (lower, upper)
-textDict = {
-    "novatel/bottom/bestpos" : (10,1000),
-    "raptor_dbw_interface/imu" : (1,100),
-    "raptor_dbw_interface/ctstate": (0,20),
-    "raptor_dbw_interface/wheel_speed_report": (10,11),
-    "raptor_dbw_interface/imu_error": (10,500),
-    "raptor_dbw_interface/gps_covariance": (100,300)
-}
-
-createTopicStatuses(textDict,objectDraw);
+def createUI(buttonsDict, textDict):
+    # create background image
+    background = Sprite("backgroundlogo",screenSizeX/2,screenSizeY*0.45,scaling=diagonal/1000,imgSource="assets/Blackandgoldlogo.png");
+    objectDraw.setBackgroundColor((0,0,0));
+    objectDraw.add(background);
 
 
 
 
 
 
+    # create pointless add-ons to make it look cooler
+    hologram1 = Sprite("hologram_rotator_1", screenSizeX*0.9,screenSizeY*0.7,diagonal/9000,"assets/hologram rotator.png");
+    hologram1.setAngularVelocity(0.5);
+    objectDraw.add(hologram1);
 
-# start the engine
-objectDraw.start();
-
-
-
-
-
-
-
+    hologram2 = Sprite("hologram_rotator_2", screenSizeX*0.07,screenSizeY*0.17,diagonal/10000,"assets/hologram_radar.jpg");
+    hologram2.setAngularVelocity(-0.7);
+    objectDraw.add(hologram2);
 
 
-# run the engine
+    # create buttons and text messages
 
 
-class msg():
-    def __init__(self,data):
-        self.data = data;
-
-
-i = 1;
-while(True):
-    i+=1;
     
-    msg1 = msg(i%500);
-    for fun in subFuncs:
-        fun(msg1);
 
-    objectDraw.run();
-    time.sleep(0.01);
+    createButtons(buttonsDict,objectDraw);
+
+    velocityButton = IAC_textBox_Button("set target velocity",objectDraw, screenSizeX*0.5,screenSizeY*0.75,200,screenSizeY*(1-screenSplitButton));
+
+    objectDraw.add(velocityButton);
+
+
+    
+    createTopicStatuses(textDict,objectDraw);
+
+
+
+
+
+
+
+    # start the engine
+    objectDraw.start();
+
+    return subFuncs;
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    # title, command
+    buttonsDict = {
+        "CT11": ("echo 'command send ct11'",""),
+        "launch lidar": ("echo 'launch lidar'","echo 'stop lidar'"),
+        "launch long control": ("echo 'long_control set true'","echo 'long_control set false'")
+    };
+
+    # topicname, (lower, upper)
+    textDict = {
+        "novatel/bottom/bestpos" : (10,1000),
+        "raptor_dbw_interface/imu" : (1,100),
+        "raptor_dbw_interface/ctstate": (0,20),
+        "raptor_dbw_interface/wheel_speed_report": (10,11),
+        "raptor_dbw_interface/imu_error": (10,500),
+        "raptor_dbw_interface/gps_covariance": (100,300)
+    }
+
+
+    subFuncs = createUI(buttonsDict,textDict);
+    # run the engine
+    class msg():
+        def __init__(self,data):
+            self.data = data;
+
+
+    i = 1;
+    while(True):
+        i+=1;
+        
+        msg1 = msg(i%500);
+        for fun in subFuncs:
+            fun(msg1);
+
+        objectDraw.run();
+        time.sleep(0.01);
 
